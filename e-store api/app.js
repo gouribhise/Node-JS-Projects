4743d9010fce5cb625371f02901edd81a98cb4ff
gouribhise <gouri.a.bhise@gmail.com>
2024-04-29 11:21:53 +0530

    setting up the beginning code in app.js

--- e-store api/app.js
+++ e-store api/app.js
@@ -1,1 +1,30 @@
-console.log('E-Commerce API');
+require('dotenv').config()
+require('express-async-errors')
+const express=require('express')
+const app=express()
+//database
+const connectDB=require('./db/connect')
+const notFoundMiddleware=require('./middleware/not-found')
+const errorHandlerMiddleware=require('./middleware/error-handler')
+//middleware
+app.use(express.json())
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+
+app.get('/',(req,res)=>{
+    res.send('e commerce api')
+})
+const PORT=process.env.PORT||5000
+const start=async()=>{
+    try{
+        await connectDB(process.env.MONGO_URL)
+        app.listen(PORT,console.log('server is listening'))
+
+    }catch(error){
+        console.log(error)
+    }
+
+}
+
+start()
