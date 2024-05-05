86832feb1cba830fd4757bcaffeca51136e99ad2
gouribhise <gouri.a.bhise@gmail.com>
2024-05-05 10:36:34 +0530

    authenticating user

---
+++ e-store api/middleware/authentication.js
@@ -0,0 +1,31 @@
+const CustomErr=require('../errors/')
+const{isTokenValid}=require('../utils')
+
+const authenticateUser=async(req,res,next)=>{
+    const token=req.signedCookies.token
+    if(!token){
+        throw new CustomErr.UnauthenticatedError('Authentication Invalid')
+    }
+    try{
+const  {name,userId,role}=isTokenValid({token})
+req.user={name,userId,role}
+ 
+next()
+    }catch(error){
+throw new CustomErr.UnauthenticatedError('Authentication Invalid')
+    }
+  
+    next()
+}
+
+const authorizePermissions=(req,res,next)=>{
+    if(req.user.role!=='admin'){
+        throw new CustomErr.UnauthorizedError('Unathorized to access this route')
+    }
+    next()
+}
+
+module.exports={
+    authenticateUser,
+    authorizePermissions
+}
--- e-store api/routes/userRoutes.js
+++ e-store api/routes/userRoutes.js
@@ -1,14 +1,15 @@
 const express=require('express')
 const router=express.Router()
+const {authenticateUser,authorizePermissions}=require('../middleware/authentication')
 const{ getAllUsers,
     getSingleUser,
     showCurrentUser,
     updateUser,
     updateUserPassword}=require('../controllers/userControllers')
 
-router.route('/').get(getAllUsers);
+router.route('/').get(authenticateUser,authorizePermissions,getAllUsers);
 router.route('/updateUser').post(updateUser)
 router.route('/updatePassword').post(updateUserPassword)
-router.route('/:id').get(getSingleUser)
+router.route('/:id').get(authenticateUser,getSingleUser)
 
 module.exports=router
